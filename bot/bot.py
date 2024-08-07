769c89104eff8e35582d0a4b8f098cec78a4ad89
tonecosys <bodyshahab2023@gmail.com>
2024-08-07 14:14:43 +0330

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,20 @@
 
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hello!')
+
+def main() -> None:
+    updater = Updater("YOUR_TELEGRAM_TOKEN")
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    updater.start_webhook(listen="0.0.0.0", port=443, url_path="YOUR_TELEGRAM_TOKEN")
+    updater.bot.set_webhook("https://your_domain.com/" + "YOUR_TELEGRAM_TOKEN")
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
